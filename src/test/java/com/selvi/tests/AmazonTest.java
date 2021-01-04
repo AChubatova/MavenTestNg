d61d79ed33f1e2c3d45637ae052d208789bff444
AChubatova <64786092+AChubatova@users.noreply.github.com>
2021-01-04 22:12:25 +0100

    Update AmazonTest.java

--- src/test/java/com/selvi/tests/AmazonTest.java
+++ src/test/java/com/selvi/tests/AmazonTest.java
@@ -21,6 +21,10 @@ public class AmazonTest {
 
     @Test
     public void testSearch(){
+	  System.out.println("##teamcity[testStarted name='MyTest.test1']")
+	  
+	  
+	  
 	  try 
             {
                 Thread.sleep(120000);
@@ -30,6 +34,8 @@ public class AmazonTest {
             // this part is executed when an exception (in this example InterruptedException) occurs
             }
 	    Assert.assertEquals("Google", "Google");
+	    System.out.println("##teamcity[testFailed name='MyTest.test1' message='failure message' details='message and stack trace']")
+	    System.out.println("##teamcity[testFinished name='MyTest.test1']")
     }
 
     @AfterTest

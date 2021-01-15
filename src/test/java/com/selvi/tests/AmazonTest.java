0a7a6d0e8f8c11a0817418d9e744585e21fa86b6
AChubatova <64786092+AChubatova@users.noreply.github.com>
2021-01-15 10:29:24 +0100

    Update AmazonTest.java

--- src/test/java/com/selvi/tests/AmazonTest.java
+++ src/test/java/com/selvi/tests/AmazonTest.java
@@ -27,7 +27,7 @@ public class AmazonTest {
 	  System.out.println("##teamcity[testStarted name='MyTest1.longTest']");
 	  try 
             {
-                Thread.sleep(1000);
+                Thread.sleep(200000);
             } 
             catch(InterruptedException e)
             {
